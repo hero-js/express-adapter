3b08dcc275f404384334322e82a4c1c7f9bbaf55
SanaYasfp <yasfp.pro@gmail.com>
2023-11-07 12:01:25 +0000

    fix: responseBuilder Generics types

--- src/class/Middleware.ts
+++ src/class/Middleware.ts
@@ -7,4 +7,18 @@ import { ExpressAdapterGenericTypes } from '../interfaces/router';
  * and 'responseBuilder', which determines the format of the responses.
  * The 'handle' method must be implemented, even if it is not necessary in a controller.
  */
-export default abstract class Middleware extends CoreMiddleware<ExpressAdapterGenericTypes> {}
+export default abstract class Middleware<
+  ApiResponse extends Record<string, any> = Record<string, any>
+> extends CoreMiddleware<ExpressAdapterGenericTypes> {
+  /**
+   * The 'responseBuilder' method determines the format of the responses.
+   * @param {ApiResponse} response - The response.
+   * @returns {string | number | ApiResponse} The response.
+   */
+  public responseBuilder(
+    response: string | number | ApiResponse
+  ): string | number | ApiResponse {
+    return response;
+  }
+}
+
